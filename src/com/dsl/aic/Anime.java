db776341690a561a63bdb4a6a3258d52c74131ed
dslsyeoh <syeoh91x@gmail.com>
2019-09-09 19:46:27 +0800

    add copyright

--- src/com/dsl/aic/Anime.java
+++ src/com/dsl/aic/Anime.java
@@ -1,3 +1,8 @@
+/*
+ * Author Steven Yeoh
+ * Copyright (c) 2019. All rights reserved.
+ */
+
 package com.dsl.aic;
 
 import java.util.Date;
--- src/com/dsl/aic/AnimeUpdater.java
+++ src/com/dsl/aic/AnimeUpdater.java
@@ -1,3 +1,8 @@
+/*
+ * Author Steven Yeoh
+ * Copyright (c) 2019. All rights reserved.
+ */
+
 package com.dsl.aic;
 
 import com.dsl.aic.utils.DateUtils;
