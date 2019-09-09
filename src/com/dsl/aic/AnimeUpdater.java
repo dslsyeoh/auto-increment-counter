2ca51296b933658f1026a3277029f98560e09359
dslsyeoh <syeoh91x@gmail.com>
2019-09-09 19:37:57 +0800

    refactor code to calculate exceeded days for auto increment counter

--- src/com/dsl/aic/AnimeUpdater.java
+++ src/com/dsl/aic/AnimeUpdater.java
@@ -13,9 +13,9 @@ class AnimeUpdater
         LocalDate nextReleaseDate = DateUtils.localDate(anime.getCurrentDate()).plusDays(anime.getNextReleaseDuration());
         LocalDate currentDate = DateUtils.localDate(new Date());
 
-        if(currentDate.equals(nextReleaseDate))
+        if(currentDate.isEqual(nextReleaseDate) || currentDate.isAfter(nextReleaseDate))
         {
-            update(nextReleaseDate, anime);
+            update(currentDate, nextReleaseDate, anime);
         }
         else
         {
@@ -23,9 +23,12 @@ class AnimeUpdater
         }
     }
 
-    private void update(LocalDate currentDate, Anime anime)
+    private void update(LocalDate currentDate, LocalDate nextReleaseDate, Anime anime)
     {
+        int counter = 0;
+        int diff = Math.abs(Period.between(currentDate, nextReleaseDate).getDays());
+        if (diff > 0) counter = diff / anime.getNextReleaseDuration();
         anime.setCurrentDate(DateUtils.toDate(currentDate));
-        anime.setCurrentEpisode(anime.getCurrentEpisode() + 1);
+        anime.setCurrentEpisode(anime.getCurrentEpisode() + 1 + counter);
     }
 }
