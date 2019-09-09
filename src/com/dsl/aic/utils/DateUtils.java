799b4dcd5e22e8edeae3ad14291f8eb08688f48b
dslsyeoh <syeoh91x@gmail.com>
2019-09-09 22:59:18 +0800

    enhance DateUtils

--- src/com/dsl/aic/utils/DateUtils.java
+++ src/com/dsl/aic/utils/DateUtils.java
@@ -5,12 +5,18 @@
 
 package com.dsl.aic.utils;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.Locale;
 
 public class DateUtils
 {
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM-dd-yyyy",Locale.ENGLISH);
+
     private DateUtils() {}
 
     public static LocalDate toLocalDate(Date date)
@@ -21,5 +27,24 @@ public class DateUtils
     public static Date toDate(LocalDate localDate)
     {
         return Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+    }
+
+    public static int daysDiff(LocalDate start, LocalDate end)
+    {
+        return Math.abs(Period.between(start, end).getDays());
+    }
+
+    public static Date toDate(String date)
+    {
+        try
+        {
+            return simpleDateFormat.parse(date);
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
